d4e297be3a0efdf6fe48d94271b7b02e4d327992
Toni Jung <tonimj92@gmail.com>
2015-10-29 18:41:26 -0700

    added comments

--- getShapefiles.py
+++ getShapefiles.py
@@ -3,10 +3,13 @@
 import urllib
 from zipfile import ZipFile
 
+# Set directory
 dir = "C:/hwacallo/"
 
+# Local variables
 urls = ["http://biogeo.ucdavis.edu/data/gadm2/gadm_v2_shp.zip"]
 
+# Download and unzip files
 def main():
     for url in urls:
         name, headers = urllib.urlretrieve(url)
